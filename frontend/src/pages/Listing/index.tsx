fb9b62aeb4f4b08e7a2bf558c257fc9b3284ad9f
Jefferson Willian <jefferson.fechia@gmail.com>
2022-01-10 22:47:52 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,11 @@
+import Pagination from "components/Pagination";
+import './styles.css';
 function Listing () {
 
     return (
-        <h1>Pagina de listagem</h1>
-    )
+        <Pagination />
+    );
+
 }
 
 export default Listing;
